f21f96a32541f67a6565769f23a0bb3fd3705629
Maciek <m43091647@gmail.com.local>
2025-04-23 22:54:29 +0200

    Zarzadzanie zamowieniami

--- frontEnd/src/components/Orderstatusmanager.vue
+++ frontEnd/src/components/Orderstatusmanager.vue
@@ -31,7 +31,7 @@
 
       <div class="mt-4 flex justify-end">
         <button
-          @click="fetchOrders"
+          @click="() => fetchOrders(0)"
           class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
           :disabled="loading"
         >
